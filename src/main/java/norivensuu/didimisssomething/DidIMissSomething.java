2e3eef8f44c1514798d5da8d52a2ee50af8adb6a
AlexNompe <Alex.Nompe@yandex.ru>
2025-03-15 15:20:48 +0300

    Simple downloader

--- src/main/java/norivensuu/didimisssomething/DidIMissSomething.java
+++ src/main/java/norivensuu/didimisssomething/DidIMissSomething.java
@@ -2,8 +2,14 @@ package norivensuu.didimisssomething;
 
 import net.fabricmc.api.ModInitializer;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
 
 public class DidIMissSomething implements ModInitializer {
 	public static final String MOD_ID = "didimisssomething";
@@ -14,5 +20,20 @@ public class DidIMissSomething implements ModInitializer {
 	public void onInitialize() {
 
 		LOGGER.info("Haiiii my little meow meows");
-	}
+
+        downloadURL("https://disk.yandex.ru/i/32uxyfj8_iDhwQ.jpg", "niko.jpg");
+    }
+
+    public static void downloadURL(String url, String saveLocation) {
+        try {
+            URL fileUrl = URI.create(url).toURL();
+            File destination = new File(saveLocation);
+
+            FileUtils.copyURLToFile(fileUrl, destination);
+
+            System.out.println("Download complete: " + destination.getAbsolutePath());
+        } catch (IOException e) {
+            System.err.println("Download failed: " + e.getMessage());
+        }
+    }
 }
