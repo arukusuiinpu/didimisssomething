de9bc85532d574611b9d6d1ee1332638b5bfafe1
AlexNompe <Alex.Nompe@yandex.ru>
2025-03-15 14:13:36 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# didimisssomething
+ 
---
+++ src/client/java/norivensuu/didimisssomething/DidIMissSomethingClient.java
@@ -0,0 +1,10 @@
+package norivensuu.didimisssomething;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DidIMissSomethingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
